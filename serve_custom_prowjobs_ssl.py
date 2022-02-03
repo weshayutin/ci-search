8b9bc56ff68d5d39e1177a0954ff800d426b3dc5
Wesley Hayutin <whayutin@redhat.com>
2022-02-03 14:14:38 -0700

    update serve_custom

--- serve_custom_prowjobs_ssl.py
+++ serve_custom_prowjobs_ssl.py
@@ -2,7 +2,7 @@
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import ssl
-httpd = HTTPServer(('localhost', 443), BaseHTTPRequestHandler)
+httpd = HTTPServer(('localhost', 8888), BaseHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket(
     httpd.socket,
     keyfile="localhost-key.pem",
