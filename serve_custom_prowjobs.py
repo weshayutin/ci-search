bd0778b6bd1a6d83c222dfa1957a5d9448e91892
Wesley Hayutin <whayutin@redhat.com>
2022-02-03 17:08:13 -0700

    update python https server

---
+++ serve_custom_prowjobs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostName = "localhost"
+hostPort = 80
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+
+myServer = HTTPServer((hostName, hostPort), MyServer)
+print(time.asctime(), "Server Starts - %s:%s" % (hostName, hostPort))
+
+try:
+    myServer.serve_forever()
+except KeyboardInterrupt:
+    pass
+
+myServer.server_close()
+print(time.asctime(), "Server Stops - %s:%s" % (hostName, hostPort))
--- serve_custom_prowjobs.sh
+++
@@ -1,8 +0,0 @@
-# serve http://localhost/prowjobs.js a smaller version than http://prow.ci.openshift.org
-# see the local prowjobs.js in this directory
-
-# execute search w/
-# ./search --path /var/tmp/oadp_ci_search --deck-uri=http://localhost  --interval 30m --v 7 --max-age 1h
-
-sudo python -m  http.server 80
-
